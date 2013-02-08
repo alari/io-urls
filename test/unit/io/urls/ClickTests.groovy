14c0d74d25bb480c41300116564285288e506107
Dmitry Kurinskiy <name.alari@gmail.com>
2013-02-09 01:06:17 +0400

    no tests yet

--- test/unit/io/urls/ClickTests.groovy
+++ test/unit/io/urls/ClickTests.groovy
@@ -12,6 +12,6 @@ import org.junit.*
 class ClickTests {
 
     void testSomething() {
-        fail "Implement me"
+        //fail "Implement me"
     }
 }
--- test/unit/io/urls/FolderTests.groovy
+++ test/unit/io/urls/FolderTests.groovy
@@ -12,6 +12,6 @@ import org.junit.*
 class FolderTests {
 
     void testSomething() {
-        fail "Implement me"
+        //fail "Implement me"
     }
 }
